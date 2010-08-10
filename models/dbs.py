dd56f06d2e3031cf58450b6fbd4fa9194bd6bbb9
Fabio Alessandro Locati <flocati@grimp.eu>
2010-08-10 18:01:54 +0200

    Now login works on GAE

--- models/dbs.py
+++ models/dbs.py
@@ -96,10 +96,10 @@ db.hours.note.label=T("Notes")
 
 from gluon.tools import Mail, Auth, Recaptcha
 
-#from gluon.contrib.login_methods.gae_google_login import GaeGoogleAccount
-#auth.settings.login_form = GaeGoogleAccount()
-#auth.settings.create_user_groups = False
 auth = Auth(globals(), db)
+from gluon.contrib.login_methods.gae_google_account import GaeGoogleAccount
+auth.settings.login_form = GaeGoogleAccount()
+auth.settings.create_user_groups = False
 ## ask it to create all necessary tables
 auth.define_tables()
 auth.settings.mailer = mail
