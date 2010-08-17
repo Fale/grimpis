51190031c86f2831cb0ce49c7234a9d0482272ac
Fabio Alessandro Locati <flocati@grimp.eu>
2010-08-17 11:54:08 +0200

    Now is possible to edit membership of users into groups from the web interface

---
+++ controllers/membership.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*- 
+
+#########################################################################
+## This is the controller for works managment
+## - index is the default action of any application
+## - user is required for authentication and authorization
+## - download is for downloading files uploaded in the db (does streaming)
+## - call exposes all registered services (none by default)
+######################################################################### 
+
+def index():
+    rows=db(db.auth_membership.id>0).select(orderby=db.auth_membership.id)
+    return dict(rows=rows)
+
+def add():
+    form=crud.create(db.auth_membership,next=url('index'))
+    return dict(form=form)
+
+def edit():
+    edit_id=request.args(0)
+    edit=db.auth_membership[edit_id]
+    form=crud.update(db.auth_membership,edit,next=url('index'))
+    return dict(form=form)
