618e6ae9f025080a29233ee6bc7918d85be3735b
Fabio Alessandro Locati <flocati@grimp.eu>
2010-08-17 12:58:24 +0200

    Better with login decorators ;)

--- controllers/groups.py
+++ controllers/groups.py
@@ -8,17 +8,17 @@
 ## - call exposes all registered services (none by default)
 ######################################################################### 
 
-#@auth.requires_permission('hr')
+@auth.requires_permission('hr')
 def index():
     rows=db(db.auth_group.id>0).select(orderby=db.auth_group.id)
     return dict(rows=rows)
 
-#@auth.requires_permission('admin')
+@auth.requires_permission('admin')
 def add():
     form=crud.create(db.auth_group,next=url('index'))
     return dict(form=form)
 
-#@auth.requires_permission('admin')
+@auth.requires_permission('admin')
 def edit():
     edit_id=request.args(0)
     edit=db.auth_group[edit_id]
