341c3078b0bc17a0822b620246a09cf91ac92ad4
Fabio Alessandro Locati <flocati@grimp.eu>
2010-08-22 22:42:42 +0200

    Fix it :), now a little bit of polish, and then I'll merge it

--- models/db.py
+++ models/db.py
@@ -1,5 +1,8 @@
 # -*- coding: utf-8 -*- 
 
+from applications.init.modules.sets import *
+is_phone = IS_MATCH('^(\+\d{2}\-)?[\d\-]*(\#\d+)?$')
+
 #########################################################################
 ## This scaffolding model makes your app work on Google App Engine too
 #########################################################################
@@ -77,3 +80,99 @@ crud.settings.auth = None                      # =auth to enforce authorization 
 ## >>> rows=db(db.mytable.myfield=='value').select(db.mytable.ALL)
 ## >>> for row in rows: print row.id, row.myfield
 #########################################################################
+
+db.define_table('customers',
+    Field('name','string'),
+    Field('piva','string'),
+    Field('email','string'),
+    Field('address_num','string'),
+    Field('address_road','string'),
+    Field('address_city','string'),
+    Field('address_county','string'),
+    Field('address_zip','string'),
+    Field('address_state','string'),
+    Field('mobile','string'),
+    Field('phone','string'),
+    Field('fax','string'))
+    
+db.customers.name.label=T("Name")
+db.customers.name.requires=[IS_NOT_EMPTY(),IS_NOT_IN_DB(db,'customers.name')]
+db.customers.piva.label=T("P IVA")
+db.customers.piva.requires=IS_NOT_EMPTY()
+db.customers.email.label=T("E-Mail")
+db.customers.address_num.label=T("Number")
+db.customers.address_road.label=T("Street")
+db.customers.address_city.label=T("City")
+db.customers.address_county.label=T("County")
+db.customers.address_zip.label=T("ZIP Code")
+db.customers.address_state.label=T("State")
+db.customers.address_state.requires = IS_IN_SET(STATES,[T(x) for x in STATES])
+db.customers.address_state.default = "Italy"
+db.customers.mobile.label=T("Mobile")
+db.customers.mobile.requires=is_phone
+db.customers.phone.label=T("Phone")
+db.customers.phone.requires=is_phone
+db.customers.fax.label=T("Fax")
+db.customers.fax.requires=is_phone
+
+
+
+db.define_table('works',
+    Field('name','string'),
+    Field('customer',db.customers),
+    Field('description','string'),
+    Field('assign_date','date'),
+    Field('due_to_date','date'),
+    Field('status','string'),
+    Field('value','integer'),
+    Field('cost','integer'))
+    
+db.works.name.label=T("Name")
+db.works.customer.label=T("Customer")
+db.works.customer.requires=IS_IN_DB(db,'customers.id','customers.name')
+db.works.description.label=T("description")
+db.works.assign_date.label=T("Assigned")
+db.works.due_to_date.label=T("Due To")
+db.works.status.label=T("Status")
+db.works.status.requires = IS_IN_SET(STATUS,[T(x) for x in STATUS])
+db.works.status.default = "Open"
+db.works.value.label=T("Value")
+db.works.cost.label=T("Cost")
+
+
+
+db.define_table('hours',
+    Field('work',db.works),
+    Field('user',db.auth_user),
+    Field('type','string'),
+    Field('start','datetime'),
+    Field('finish','datetime'),
+    Field('note','string'))
+    
+db.hours.work.label=T("Work")
+db.hours.work.requires=IS_IN_DB(db,'works.id','%(name)s')
+db.hours.user.label=T("Worker")
+db.hours.user.requires=IS_IN_DB(db,'auth_user.id','%(first_name)s %(last_name)s')
+db.hours.type.requires = IS_IN_SET(TYPES,[T(x) for x in TYPES])
+db.hours.type.default = "Coding"
+db.hours.start.label=T("Starting time")
+db.hours.finish.label=T("Finishing time")
+db.hours.note.label=T("Notes")
+
+###DEFAULT VALUES
+#if not db(db.auth_group.id>0).count():
+#    admin=auth.add_group('admin', 'can access to all actions')
+#    auth.add_permission(admin, 'admin')
+#    auth.add_permission(admin, 'manager')
+#    auth.add_permission(admin, 'hr')
+#    auth.add_permission(admin, 'crew')
+#    manager=auth.add_group('manager', 'can access to managerial actions')
+#    auth.add_permission(manager, 'manager')
+#    auth.add_permission(manager, 'crew')
+#    hr=auth.add_group('hr', 'can access to all human resources action')
+#    auth.add_permission(hr, 'hr')
+#    auth.add_permission(hr, 'crew')
+#    crew=auth.add_group('crew', 'can access to all basic action')
+#    auth.add_permission(crew, 'crew')
+#if not db(db.auth_membership.id>0).count():
+#    auth.add_membership(1, 1)
